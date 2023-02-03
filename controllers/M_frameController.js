65f8963da617591fa36266f8ebbeb68b6cdd6b70
PoopEuy <mrakajanuarsyah@gmail.com>
2023-02-03 10:35:47 +0700

    update mframe controller

--- controllers/M_frameController.js
+++ controllers/M_frameController.js
@@ -1,4 +1,5 @@
 import { where } from "sequelize";
+import { Sequelize } from "sequelize";
 import M_frame from "../models/M_frameModel.js";
 
 export const getMframe = async (req, res) => {
@@ -25,10 +26,11 @@ export const getMframeById = async (req, res) => {
 
 export const getMframByFrame = async (req, res) => {
   try {
+    //iilke digunakan agar uppercase dan lowercase tidak pengaruh
+    const frame_sn = req.body.frame_sn;
+    console.log("tes : " + frame_sn);
     const response = await M_frame.findOne({
-      where: {
-        frame_sn: req.body.frame_sn,
-      },
+      where: { frame_sn: { [Sequelize.Op.iLike]: `%${frame_sn}%` } },
     });
 
     res.status(200).json({ msg: "sucess", data: response });
@@ -65,14 +67,32 @@ export const updateMframeById = async (req, res) => {
 };
 
 export const updateMframeByFrame = async (req, res) => {
+  // try {
+  //   const response = await M_frame.update(req.body, {
+  //     where: {
+  //       frame_sn: req.body.frame_sn,
+  //     },
+  //   });
+
+  //   res.status(200).json({ msg: "mframe Updated" });
+  // } catch (error) {
+  //   console.log(error.message);
+  // }
+
   try {
+    const frame_sn = req.body.frame_sn;
+    const result = req.body.result;
     const response = await M_frame.update(req.body, {
-      where: {
-        frame_sn: req.body.frame_sn,
-      },
+      where: { frame_sn: frame_sn },
     });
 
-    res.status(200).json({ msg: "mframe Updated" });
+    if (response == 1) {
+      console.log("hasil = " + response);
+      res.status(200).json({ msg: "update success" });
+    } else {
+      console.log("hasil = " + response);
+      res.status(200).json({ msg: "update failed" });
+    }
   } catch (error) {
     console.log(error.message);
   }
--- routes/CMSRoute.js
+++ routes/CMSRoute.js
@@ -1,8 +1,9 @@
 import express from "express";
-import { setFrameCMS } from "../controllers/CMSController.js";
+import { setFrameCMS, restartCMS } from "../controllers/CMSController.js";
 
 const router = express.Router();
 
 router.post("/setFrameCMS", setFrameCMS);
+router.post("/restartCMS", restartCMS);
 
 export default router;
